301a677c579eda52fb610584a43a95b55cdbe16e
perpussemangatpagi <perpussemangatpagi@gmail.com>
2026-06-28 14:52:47 +0800

    Delete api/auth.js

--- api/auth.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  const client_id = process.env.GITHUB_CLIENT_ID;
-  const redirect_uri = 'https://smpn1damai.web.id/api/callback';
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo,user`);
-}
